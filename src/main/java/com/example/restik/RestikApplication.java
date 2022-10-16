ed937856bc8c0dd22e0f2ca03ad7f00043956d39
shapyshka <shapyshka2002@gmail.com>
2022-10-16 20:02:54 +0300

    Header/footer fixes

--- src/main/java/com/example/restik/RestikApplication.java
+++ src/main/java/com/example/restik/RestikApplication.java
@@ -2,9 +2,10 @@ package com.example.restik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestikApplication {
+public class RestikApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestikApplication.class, args);
